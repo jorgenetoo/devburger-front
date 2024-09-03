4b6c764b1e123a4d265a5958dd8966322ddf6c21
jorgenetoo <jorgenetoo@yahoo.com.br>
2024-09-03 09:46:45 -0300

    Importação dos Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import { ToastContainer } from 'react-toastify';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyles from './styles/globalStyles';
 import { router } from './routes';
-import { UserProvider } from './hooks/UserContext';
+import AppProvider from './hooks';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
+    <AppProvider>
       <RouterProvider router={router}>
         <GlobalStyles />
         <ToastContainer autoClose={2000} theme="colored" />
       </RouterProvider>
-    </UserProvider>
+    </AppProvider>
   </React.StrictMode>,
 );
