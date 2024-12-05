c78fd7f94e52a05a4e5e9c494fbe1668ed49fc36
jorgenetoo <jorgenetoo@yahoo.com.br>
2024-12-05 15:28:23 -0300

    Finalizado carrinho

--- src/components/CartItems/index.jsx
+++ src/components/CartItems/index.jsx
@@ -6,8 +6,8 @@ import formatCurrency from "../../utils/formatCurrency.jsx";
 
 
 export function CartItems() {
-    const { cartProducts , increaseProducts, decreaseProducts} = useCart()
-    console.log(cartProducts)
+    const { cartProducts , increaseProductQuantity, decreaseProductQuantity} = useCart()
+    
     return (
         <Container>
             <Header>
@@ -25,9 +25,9 @@ export function CartItems() {
                         <p>{product.name}</p>
                         <p>{formatCurrency(product.price)}</p>
                         <div className="quantity-container">
-                            <button onClick={()=> decreaseProducts(product.id)}>-</button>
+                            <button onClick={()=> decreaseProductQuantity(product.id)}>-</button>
                             <p>{product.quantity}</p>
-                            <button onClick={() => increaseProducts(product.id)}>+</button>
+                            <button onClick={() => increaseProductQuantity(product.id)}>+</button>
                         </div>
                         <p>{formatCurrency(product.quantity * product.price)}</p>
 
--- src/hooks/CartContext.jsx
+++ src/hooks/CartContext.jsx
@@ -1,121 +1,103 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-import PropTypes from 'prop-types'
-
-
-const CartContext = createContext({})
+const CartContext = createContext({});
 
 export const CartProvider = ({ children }) => {
-    const [cartProducts, setCartProducts] = useState([])
+  const [cartProducts, setCartProducts] = useState([]);
 
-    const updateLocalStorage = async (products) => {
-        await localStorage.setItem(
-            'codeburger:cartInfo',
-            JSON.stringify(products)
-        )
+  const updateLocalStorage = (products) => {
+    localStorage.setItem("codeburger:cartInfo", JSON.stringify(products));
+  };
+
+  const putProductInCart = (product) => {
+    const cartIndex = cartProducts.findIndex((prd) => prd.id === product.id);
+    let newCartProducts = [...cartProducts];
+
+    if (cartIndex >= 0) {
+      newCartProducts[cartIndex] = {
+        ...newCartProducts[cartIndex],
+        quantity: newCartProducts[cartIndex].quantity + 1,
+      };
+    } else {
+      newCartProducts.push({ ...product, quantity: 1 });
     }
 
-    const putProductInCart = async product => {
-        const cartIndex = cartProducts.findIndex(prd => prd.id === product.id)
+    setCartProducts(newCartProducts);
+    updateLocalStorage(newCartProducts);
+  };
 
-        let newCartProducts = []
-        if (cartIndex >= 0) {
-            newCartProducts = cartProducts
+  const deleteProduct = (productId) => {
+    const newCart = cartProducts.filter((product) => product.id !== productId);
+    setCartProducts(newCart);
+    updateLocalStorage(newCart);
+  };
 
-            newCartProducts[cartIndex].quantity =
-                newCartProducts[cartIndex].quantity + 1
-            setCartProducts(newCartProducts)
+  const increaseProductQuantity = (productId) => {
+    const newCart = cartProducts.map((product) =>
+      product.id === productId
+        ? { ...product, quantity: product.quantity + 1 }
+        : product
+    );
+    setCartProducts(newCart);
+    updateLocalStorage(newCart);
+  };
 
-        } else {
-            product.quantity = 1
-            newCartProducts = ([...cartProducts, product])
-            setCartProducts(newCartProducts)
-        }
+  const decreaseProductQuantity = (productId) => {
+    const product = cartProducts.find((p) => p.id === productId);
+    if (!product) return;
 
-        await updateLocalStorage(newCartProducts)
-
+    if (product.quantity > 1) {
+      const newCart = cartProducts.map((p) =>
+        p.id === productId ? { ...p, quantity: p.quantity - 1 } : p
+      );
+      setCartProducts(newCart);
+      updateLocalStorage(newCart);
+    } else {
+      deleteProduct(productId);
     }
+  };
 
-    const clearCart =  () =>{
-        setCartProducts([])
-        updateLocalStorage([])
-    }
+  const clearCart = () => {
+    setCartProducts([]);
+    updateLocalStorage([]);
+  };
 
-    const deleteProducts = async productId => {
-        const newCart = cartProducts.filter(product => product.id != productId)
+  useEffect(() => {
+    const loadUserData = () => {
+      const clientCartData = localStorage.getItem("codeburger:cartInfo");
+      if (clientCartData) {
+        setCartProducts(JSON.parse(clientCartData));
+      }
+    };
+    loadUserData();
+  }, []);
 
-        setCartProducts(newCart)
-
-        await updateLocalStorage(newCart)
-
-    }
-
-    const increaseProducts = async productId => {
-        const newCart = cartProducts.map(product => {
-            return product.id === productId ? { ...product, quantity: product.quantity + 1 } : product
-
-        })
-        setCartProducts(newCart)
-
-        await updateLocalStorage(newCart)
-
-    }
-
-
-    const decreaseProducts = async productId => {
-        const cartIndex = cartProducts.findIndex(pd => pd.id === productId)
-
-        if (cartProducts[cartIndex].quantity > 1) {
-
-            const newCart = cartProducts.map(product => {
-                return product.id === productId ? { ...product, quantity: product.quantity - 1 } : product
-
-            })
-            setCartProducts(newCart)
-
-            await localStorage.setItem(
-                'codeburger:cartInfo',
-                JSON.stringify(newCart)
-            )
-        } else {
-            deleteProducts(productId)
-        }
-
-
-    }
-
-
-    useEffect(() => {
-        const loadUserDate = async () => {
-            const clienCartData = await localStorage.getItem('codeburger:cartInfo')
-
-            if (clienCartData) {
-                setCartProducts(JSON.parse(clienCartData))
-            }
-
-        }
-        loadUserDate()
-
-    }, [])
-
-    return (
-        <CartContext.Provider value={{ putProductInCart, cartProducts, increaseProducts, decreaseProducts }}>
-            {children}
-        </CartContext.Provider>
-    )
-
-}
+  return (
+    <CartContext.Provider
+      value={{
+        cartProducts,
+        putProductInCart,
+        clearCart,
+        deleteProduct,
+        increaseProductQuantity,
+        decreaseProductQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 export const useCart = () => {
-    const context = useContext(CartContext)
-
-    if (!context) {
-        throw new Error('useCart must be used with UserContext')
-    }
-
-    return context
-}
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used with CartProvider");
+  }
+  return context;
+};
 
 CartProvider.propTypes = {
-    children: PropTypes.node
-}
+  children: PropTypes.node.isRequired,
+};
+
